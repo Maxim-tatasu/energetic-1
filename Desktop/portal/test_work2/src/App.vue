f7b80e044ba01e5af109cc7db6c7e34e77a85ae2
Maxim <maksimka.plotnikov.2000@list.ru>
2021-06-10 17:34:55 +0300

    relize v 2.1

---
+++ Desktop/portal/test_work2/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <game/>
+</div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import game from './components/game.vue'
+//import MedForm from './components/MedForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+   game,
+   // MedForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family:  Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
